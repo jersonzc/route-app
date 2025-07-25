8b78db5fbee54592598bd84633b1ded85d31f4bd
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-07-25 11:32:15 -0500

    refactor: rename struct name

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,22 +5,22 @@ import (
 	"route-app/internal/route"
 )
 
-type routeServer struct {
+type application struct {
 	route.UnimplementedRouteServer
 }
 
-func (s *routeServer) GetFeature(ctx context.Context, point *route.Point) (*route.Feature, error) {
+func (s *application) GetFeature(ctx context.Context, point *route.Point) (*route.Feature, error) {
 	return nil, nil
 }
 
-func (s *routeServer) ListFeatures(rect *route.Rectangle, stream route.Route_ListFeaturesServer) error {
+func (s *application) ListFeatures(rect *route.Rectangle, stream route.Route_ListFeaturesServer) error {
 	return nil
 }
 
-func (s *routeServer) RecordRoute(stream route.Route_RecordRouteServer) error {
+func (s *application) RecordRoute(stream route.Route_RecordRouteServer) error {
 	return nil
 }
 
-func (s *routeServer) RouteChat(stream route.Route_RouteChatServer) error {
+func (s *application) RouteChat(stream route.Route_RouteChatServer) error {
 	return nil
 }
