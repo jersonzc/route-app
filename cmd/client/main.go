eae658b90d7e3d5f85c296d0ede6880fb4d7cc40
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-08-01 15:37:11 -0500

    feat: print features in rectangle area

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -5,11 +5,13 @@ import (
 	"flag"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"io"
 	"log"
 	"route-app/internal/route"
 	"time"
 )
 
+// printFeature gets the feature for the given point.
 func printFeature(client route.RouteClient, point *route.Point) {
 	log.Printf("Getting feature for point (%d, %d)", point.Latitude, point.Longitude)
 
@@ -22,6 +24,31 @@ func printFeature(client route.RouteClient, point *route.Point) {
 	}
 
 	log.Println(feature)
+}
+
+// printFeatures lists all the features within the given bounding Rectangle.
+func printFeatures(client route.RouteClient, rect *route.Rectangle) {
+	log.Printf("Looking for features within %v", rect)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	stream, err := client.ListFeatures(ctx, rect)
+	if err != nil {
+		log.Fatalf("client.ListFeatures failed: %v", err)
+	}
+
+	for {
+		feature, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("client.ListFeatures failed: %v", err)
+		}
+
+		log.Printf("Feature: name: %q, point:(%v, %v)", feature.GetName(), feature.GetLocation().GetLatitude(), feature.GetLocation().GetLongitude())
+	}
 }
 
 func main() {
@@ -43,4 +70,10 @@ func main() {
 
 	// Feature missing.
 	printFeature(srv, &route.Point{Latitude: 0, Longitude: 0})
+
+	// Looking for features between 40, -75 and 42, -73.
+	printFeatures(srv, &route.Rectangle{
+		Lo: &route.Point{Latitude: 400000000, Longitude: -750000000},
+		Hi: &route.Point{Latitude: 420000000, Longitude: -730000000},
+	})
 }
