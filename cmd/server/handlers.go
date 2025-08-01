c50fe7f787689b68f2cec24d8dbef1ba86a1d8fc
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-08-01 15:00:20 -0500

    refactor: receiver name

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -5,19 +5,19 @@ import (
 	"route-app/internal/route"
 )
 
-func (s *application) GetFeature(ctx context.Context, point *route.Point) (*route.Feature, error) {
-	s.infoLog.Printf("received: %v", point.String())
+func (app *application) GetFeature(ctx context.Context, point *route.Point) (*route.Feature, error) {
+	app.infoLog.Printf("received: %v", point.String())
 	return &route.Feature{Name: "test"}, nil
 }
 
-func (s *application) ListFeatures(rect *route.Rectangle, stream route.Route_ListFeaturesServer) error {
+func (app *application) ListFeatures(rect *route.Rectangle, stream route.Route_ListFeaturesServer) error {
 	return nil
 }
 
-func (s *application) RecordRoute(stream route.Route_RecordRouteServer) error {
+func (app *application) RecordRoute(stream route.Route_RecordRouteServer) error {
 	return nil
 }
 
-func (s *application) RouteChat(stream route.Route_RouteChatServer) error {
+func (app *application) RouteChat(stream route.Route_RouteChatServer) error {
 	return nil
 }
