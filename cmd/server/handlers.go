c375608e15f94fb55a1748458d523b1d435f1cfa
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-08-01 15:27:04 -0500

    feat: implement GetFeature

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"google.golang.org/protobuf/proto"
+	"os"
 	"route-app/internal/route"
 )
 
 func (app *application) GetFeature(ctx context.Context, point *route.Point) (*route.Feature, error) {
-	app.infoLog.Printf("received: %v", point.String())
-	return &route.Feature{Name: "test"}, nil
+	for _, feature := range app.savedFeatures {
+		if proto.Equal(feature.Location, point) {
+			return feature, nil
+		}
+	}
+	return &route.Feature{Location: point}, nil
 }
 
 func (app *application) ListFeatures(rect *route.Rectangle, stream route.Route_ListFeaturesServer) error {
