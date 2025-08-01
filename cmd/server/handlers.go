6ad2a903f3014bf840965d5c30ce7d78da63f4f9
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-08-01 15:47:53 -0500

    feat: implement route summary

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -6,9 +6,11 @@ import (
 	"errors"
 	"fmt"
 	"google.golang.org/protobuf/proto"
+	"io"
 	"math"
 	"os"
 	"route-app/internal/route"
+	"time"
 )
 
 func (app *application) GetFeature(ctx context.Context, point *route.Point) (*route.Feature, error) {
@@ -32,7 +34,34 @@ func (app *application) ListFeatures(rect *route.Rectangle, stream route.Route_L
 }
 
 func (app *application) RecordRoute(stream route.Route_RecordRouteServer) error {
-	return nil
+	var pointCount, featureCount, distance int32
+	var lastPoint *route.Point
+	startTime := time.Now()
+	for {
+		point, err := stream.Recv()
+		if err == io.EOF {
+			endTime := time.Now()
+			return stream.SendAndClose(&route.RouteSummary{
+				PointCount:   pointCount,
+				FeatureCount: featureCount,
+				Distance:     distance,
+				ElapsedTime:  int32(endTime.Sub(startTime).Seconds()),
+			})
+		}
+		if err != nil {
+			return err
+		}
+		pointCount++
+		for _, feature := range app.savedFeatures {
+			if proto.Equal(feature.Location, point) {
+				featureCount++
+			}
+		}
+		if lastPoint != nil {
+			distance += calcDistance(lastPoint, point)
+		}
+		lastPoint = point
+	}
 }
 
 func (app *application) RouteChat(stream route.Route_RouteChatServer) error {
@@ -70,4 +99,29 @@ func inRange(point *route.Point, rect *route.Rectangle) bool {
 		return true
 	}
 	return false
+}
+
+func toRadians(num float64) float64 {
+	return num * math.Pi / float64(180)
+}
+
+// calcDistance calculates the distance between two points using the "haversine" formula.
+// The formula is based on http://mathforum.org/library/drmath/view/51879.html.
+func calcDistance(p1 *route.Point, p2 *route.Point) int32 {
+	const CordFactor float64 = 1e7
+	const R = float64(6371000) // earth radius in metres
+	lat1 := toRadians(float64(p1.Latitude) / CordFactor)
+	lat2 := toRadians(float64(p2.Latitude) / CordFactor)
+	lng1 := toRadians(float64(p1.Longitude) / CordFactor)
+	lng2 := toRadians(float64(p2.Longitude) / CordFactor)
+	dlat := lat2 - lat1
+	dlng := lng2 - lng1
+
+	a := math.Sin(dlat/2)*math.Sin(dlat/2) +
+		math.Cos(lat1)*math.Cos(lat2)*
+			math.Sin(dlng/2)*math.Sin(dlng/2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+
+	distance := R * c
+	return int32(distance)
 }
